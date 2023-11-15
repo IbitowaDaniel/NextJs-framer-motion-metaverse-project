2831f9cfe4a23f0e7ccc85223ff44a71d767aed0
Ibitowa Daniel <ibitowadanielfavour@gmail.com>
2023-11-14 21:02:07 -0800

    edited files

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <body className='bg-primary-black overflow--hidden'>
+      <body className='bg-primary-black overflow-hidden'>
         <Navbar />
         {children}
         <Footer />
